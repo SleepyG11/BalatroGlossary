bb0ce9a952e8310adf997f3cada20ad88b6b337c
SleepyG11 <makatymba2001@yandex.ru>
2026-07-23 22:49:28 +0300

    Display planet for poker hands

--- src/ui/sections/poker_hands.lua
+++ src/ui/sections/poker_hands.lua
@@ -57,6 +57,39 @@ function Glossary.UI.simple_poker_hand(handname, simple, in_collection)
 				nodes = new_nodes,
 			},
 		}
+
+		local _planet
+		for k, v in pairs(G.P_CENTER_POOLS.Planet) do
+			if v.config.hand_type == handname then
+				_planet = v.key
+			end
+		end
+
+		if _planet then
+			local planet_area = CardArea(0, 0, 0.9, 0.9, { highlight_limit = 0, type = "title_2", collection = true })
+			local card = Glossary.safe_card_from_center(_planet, planet_area)
+			card:hard_set_T(0, 0, G.CARD_W / 2.25, G.CARD_H / 2.25)
+			planet_area:emplace(card)
+			return {
+				n = G.UIT.R,
+				config = { colour = { 0, 0, 0, 0.1 }, r = 0.25, align = "cm" },
+				nodes = {
+					{
+						n = G.UIT.C,
+						nodes = {
+							vanilla_ui,
+						},
+					},
+					{
+						n = G.UIT.O,
+						config = {
+							object = planet_area,
+						},
+					},
+				},
+			}
+		end
+
 		return vanilla_ui
 	end
 end
